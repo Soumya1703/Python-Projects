052d8b419e8bd336cb11356a6a0ec6d54b774e18
Soumya1703 <mondalsoumya570@gmail.com>
2024-10-06 02:36:45 +0530

    Initial Comit

---
+++ 1. Guess the Number.py
@@ -0,0 +1,23 @@
+import random
+
+number = random.randint(0, 10)
+
+print("Welcome to the Number Guessing Game!")
+print("I have selected a number between 0 and 100. Can you guess what it is?")
+
+while True:
+    user_input = None
+    try:
+        user_input = int(input("Please guess the number: "))
+    except ValueError as e:
+        print("Please enter a valid number")
+        continue
+    if user_input < number:
+        print("Too Low. Please try again.")
+
+    elif user_input > number:
+        print("Too High. Please try again.")
+
+    else:
+        print("Congratulation!!! You guessed the number correctly.")
+        break
