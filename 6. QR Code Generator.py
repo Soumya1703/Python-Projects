76a8f78c9ac98a689f664fcdd4b78a36942a645a
Soumya1703 <mondalsoumya570@gmail.com>
2024-10-21 21:18:06 +0530

    Final Push

--- 6. QR Code Generator.py
+++ 6. QR Code Generator.py
@@ -1,5 +1,6 @@
 import qrcode
 
+
 # Function to generate a QR code
 def generate_qr(data, file_name="qrcode.png"):
     # Create QR code object
@@ -9,17 +10,18 @@ def generate_qr(data, file_name="qrcode.png"):
         box_size=10,  # Size of each box in pixels
         border=4,  # Thickness of the border
     )
-    
+
     # Add data to the QR code
     qr.add_data(data)
     qr.make(fit=True)
 
     # Create an image from the QR code
-    img = qr.make_image(fill='black', back_color='white')
-    
+    img = qr.make_image(fill="black", back_color="white")
+
     # Save the image file (overwriting the previous one)
     img.save(file_name)
     print(f"QR code saved as {file_name}")
+
 
 # Ask the user for the input data
 data = input("Enter the data or URL to be encoded in the QR code: ")
